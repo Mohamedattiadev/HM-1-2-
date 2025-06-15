fc6467cc3d2f0540d11495f5b5220dd0e2109c2f
MohamedattiaDev <mohamedattia.dev@gmail.com>
2025-06-16 00:27:23 +0300

    this is a small commit to try zed git

--- Internship(HM Section 2)/src/App.vue
+++ Internship(HM Section 2)/src/App.vue
@@ -2,19 +2,19 @@
 import NavbarComponent from "./components/NavbarComponent.vue";
 </script>
 <template>
-  <v-app>
-    <!-- Top App Bar -->
-
-    <NavbarComponent />
-    <v-main class="bg-grey-lighten-5">
-      <router-view />
-    </v-main>
-  </v-app>
+    <v-app>
+        <!-- Top App Bar -->
+        //components
+        <NavbarComponent />
+        <v-main class="bg-grey-lighten-5">
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <style>
 body {
-  margin: 0;
-  padding: 0;
+    margin: 0;
+    padding: 0;
 }
 </style>
